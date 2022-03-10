fcb056c8f3c25eb0670b5f31b15924e4752d667d
RemiGolec <83162017+RemiGolec@users.noreply.github.com>
2022-03-10 21:44:18 +0000

    City Search Component tests added

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -3,10 +3,16 @@ import React, { Component } from 'react';
 class CitySearch extends Component {
     render() {
         return (
-            <div>
+            <div className="CitySearch">
+                <input
+                    type="text"
+                    className="city"
+                />
+                <ul className="suggestions">
 
+                </ul>
             </div>
-        )
+        );
     }
 }
 
---
+++ src/__tests__/CitySearch.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CitySearch from '../CitySearch';
+
+describe('<CitySearch /> component', () => {
+    test('render text input', () => {
+        const CitysearchWrapper = shallow(<CitySearch />);
+        expect(CitysearchWrapper.find('.city')).toHaveLength(1);
+    });
+    test('render a list of suggestions', () => {
+        const CitySearchWrapper = shallow(<CitySearch />);
+        expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
+    });
+});
+
+
