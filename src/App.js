2a7ca54b03715445f20311a36e068e64996d61b2
RemiGolec <83162017+RemiGolec@users.noreply.github.com>
2022-03-13 21:38:18 +0000

    added collapsing event details

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
+import { mockData } from './mock-data';
 
 function App() {
   return (
     <div className="App">
       <CitySearch />
-      <EventList />
+      <EventList events={mockData} />
     </div>
   );
 }
--- src/Event.js
+++ src/Event.js
@@ -1,13 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
 
 class Event extends Component {
-    render() {
-        return
-        <div>
-
-        </div>;
+    state = {
+        collapsed: true
     }
+
+    handleClick = () => {
+        this.setState({
+            collapsed: !this.state.collapsed
+        })
+    }
+
+
+    render() {
+
+        const { event } = this.props;
+        const { collapsed } = this.state;
+        return (
+            <>
+                <div>
+                    <p>Events Title: {event.summary}</p>
+                    <button
+                        onClick={this.handleClick}>
+                        {collapsed ? "Show event details" : "Hide event details"}
+                    </button>
+                </div>
+                {!collapsed &&
+                    <div>
+                        <p>Event Details: {event.description}</p>
+                    </div>
+                }
+            </>
+
+        )
+    }
+
+
 }
 
 export default Event;
-
