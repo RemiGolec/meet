a75c212b225d5895098e6da3025b2bdf93f54878
RemiGolec <83162017+RemiGolec@users.noreply.github.com>
2022-03-30 21:02:23 +0100

    added nprogress css

--- src/api.js
+++ src/api.js
@@ -8,14 +8,68 @@
  */
 
 import { mockData } from './mock-data';
+import axios from 'axios';
 
-export const getEvents = async () => {
-    return mockData;
+const checkToken = async (accessToken) => {
+    const result = await fetch(
+        `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+    )
+        .then((res) => res.json())
+        .catch((error) => error.json());
+
+    return result;
 }
 
+//------  NProgress function (coming from nprogress Node package) is used to create and display progress bars
+export const getEvents = async () => {
+    NProgress.start();
+
+    if (window.location.href.startsWith('http://localhost')) {
+        NProgress.done();
+        return mockData;
+    }
+
+    const token = await getAccessToken();
+
+    if (token) {
+        removeQuery();
+        const url = `https://w895d88ot6.execute-api.eu-central-1.amazonaws.com/dev/api/get-events/{access_token}`;
+        const result = await axios.get(url);
+        if (result.data) {
+            var locations = extractLocations(result.data.events);
+            localStorage.setItem("lastEvents", JSON.stringify(result.data));
+            localStorage.setItem("locations", JSON.stringify(locations));
+        }
+        NProgress.done();
+        return result.data.events;
+    }
+}
 
 export const extractLocations = (events) => {
     var extractLocations = events.map((event) => event.location);
     var locations = [...new Set(extractLocations)];
     return locations;
 };
+
+export const getAccessToken = async () => {
+
+};
+
+const accessToken = localStorage.getItem('access_token');
+
+const tokenCheck = accessToken && (await checkToken(accessToken));
+
+if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+        const results = await axios.get(
+            "https://w895d88ot6.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+        );
+        const { authUrl } = results.data;
+        return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+}
+return accessToken;
