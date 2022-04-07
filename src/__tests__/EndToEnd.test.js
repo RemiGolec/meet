70c96b5ad07c0adfee7497630991405664386860
RemiGolec <83162017+RemiGolec@users.noreply.github.com>
2022-04-07 14:50:56 +0100

    acceptance tests for filterEventsByCity/showHidewEventsDetails/specifyNumberOfEvents

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -4,15 +4,16 @@ describe('show/hide an event details', () => {
     let browser;
     let page;
     beforeAll(async () => {
-        jest.setTimeout(30000);
+        jest.setTimeout(100000);
         browser = await puppeteer.launch({
             headless: false,
-            slowMo: 250, // slow down by 250ms
+            // slowMo: 250, // slow down by 250ms
             ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
         });
         page = await browser.newPage();
-        await page.goto('http://localhost:3000/meet');
+        await page.goto('http://localhost:3000/');
         await page.waitForSelector('.event');
+
     });
 
     afterAll(() => {
@@ -29,4 +30,11 @@ describe('show/hide an event details', () => {
         const eventDetails = await page.$('.event .event-description');
         expect(eventDetails).toBeDefined();
     });
+
+    test('User can collapse an event to hide its details', async () => {
+        await page.click('.event .btn-details');
+        const eventDetails = await page.$('.event .event__Details');
+        expect(eventDetails).toBeNull();
+    });
+
 });
